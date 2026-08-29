bcd2ba721ec9c517c773f1e51f161e40c91291da
elitedev42101 <foxofdesert421@outlook.com>
2026-08-29 05:37:24 -0400

    feat

--- tests/test_agents_config.py
+++ tests/test_agents_config.py
@@ -126,13 +126,6 @@ agent_name:
         
         empty_agent_names = get_all_agent_names()
         self.assertEqual(empty_agent_names, [])
-
-    def test_get_agent(self):
-        shutil.copy(BASE_PATH / "fixtures/agents_max.yaml", self.project_dir / AGENTS_FILENAME)
-
-        agent = get_agent("agent_name")
-        self.assertIsInstance(agent, AgentConfig)
-        self.assertEqual(agent.name, "agent_name")
     
     def test_get_agent_prompt(self):
         shutil.copy(BASE_PATH / "fixtures/agents_max.yaml", self.project_dir / AGENTS_FILENAME)
